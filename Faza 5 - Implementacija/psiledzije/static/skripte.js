aa5cfa2e6f74b5c2df47e6711b0892ff0920dbb7
ljubicamuravljov <ml200071d@student.etf.bg.ac.rs>
2023-05-29 16:57:56 +0200

    fuk
    Change-Id: I5f0c013d80dbce9c18b65ad0a540d9c3b7782b50

--- Faza 5 - Implementacija/psiledzije/static/skripte.js
+++ Faza 5 - Implementacija/psiledzije/static/skripte.js
@@ -22,5 +22,88 @@ function showModalDelete(idRec) {
 
     $("#hiddenIdDeleteRec").val(idRec)
     $("#deleteRecenzijaModal").show()
+}
+
+
+function popUpPretpl()
+{
+    alert("Pretplatili ste se na autora");  
+}
+
+function dodajRec()
+{
+
+    $("#popupRecenzija").toggle();
+
+    // var popupDiv = document.getElementById("popupRecenzija");
+    // if (popupDiv.style.display == "none") {
+    //     popupDiv.style.display = "block";
+    // } else {
+    //     popupDiv.style.display = "none";
+    // }
+
+
+}
+
+
+// document.getElementById("dodajRec").addEventListener("click", function() {
+//
+// });
+function editRec(button,tekst,ocena){
+
+
+    $("#editRec").toggle();
+
+    $("#hiddenIdRec").val(button.value);
+    $("#editTeksta").val(tekst);
+    $("#editOcena").val(ocena);
+    // var idRec=document.getElementById("hiddenIdRec");
+    // idRec.value=button.value;
+    //
+    // var editTekst=document.getElementById("editTeksta");
+    // editTekst.value= tekst;
+    //
+    // var editOcena=document.getElementById("editOcena");
+    // editOcena.value=ocena;
+
+}
+function meniRec()
+{
+    var pop=document.getElementById("dropDownRec");
+    pop.style="display:block; padding:15px; "
+}
+
+function ukloniMeni()
+{
+    $("#dropDownRec").hide();
+}
+
+function editObjavu(){
+    var pop=document.getElementById("editObj")
+    pop.style="display:block; padding:15px; "
+}
+
+function uploadEditObj(){
+    var pop=document.getElementById("editObj")
+    pop.style="display:none;"
+}
+
+function toggleModal() {
+    $(".modal").toggle()
+}
+
+
+function showModalEdit(button, tekst, ocena) {
+    $(".modalEdit").show()
+    var idRec=document.getElementById("hiddenIdRec")
+    idRec.value=button.value
+    var editTekst=document.getElementById("editTeksta")
+    editTekst.value= tekst
+    var editOcena=document.getElementById("editOcena")
+    editOcena.value=ocena
+}
+
+function hideModalEdit() {
+    $(".modalEdit").hide()
 
 }
