daee289c8aae631cd4335e58a1da5979a95daf79
AkileePikilee <ma200282d@student.etf.bg.ac.rs>
2023-05-30 17:04:33 +0200

    Carousel fix index
    Change-Id: Ib041eee229089ce9659f3a41822317975621aaf7

--- Faza 5 - Implementacija/psiledzije/static/skripte.js
+++ Faza 5 - Implementacija/psiledzije/static/skripte.js
@@ -16,54 +16,54 @@ $(document).ready(function () {
             {
                 interval: false
             })
-
         //licna kolekcija
-        var carouselWidthLicna = $(".tripleCarousel.licna .carousel-inner")[0].scrollWidth;
-        var cardWidthLicna = $(".tripleCarousel.licna .carousel-item").width();
+        if ($(".tripleCarousel.licna .carousel-inner")) {
+            var carouselWidthLicna = $(".tripleCarousel.licna .carousel-inner")[0].scrollWidth;
+            var cardWidthLicna = $(".tripleCarousel.licna .carousel-item").width();
 
-        scrollPositionLicna = 0;
+            scrollPositionLicna = 0;
 
-        $(".tripleCarousel.licna .carousel-control-next").on("click", function () {
-            if (scrollPositionLicna < (carouselWidthLicna - cardWidthLicna * 4)) { //check if you can go any further
-                scrollPositionLicna += cardWidthLicna;  //update scroll position
-                $(".tripleCarousel.licna .carousel-inner").animate({scrollLeft: scrollPositionLicna}, 600); //scroll left
-            }
-        });
+            $(".tripleCarousel.licna .carousel-control-next").on("click", function () {
+                if (scrollPositionLicna < (carouselWidthLicna - cardWidthLicna * 4)) { //check if you can go any further
+                    scrollPositionLicna += cardWidthLicna;  //update scroll position
+                    $(".tripleCarousel.licna .carousel-inner").animate({scrollLeft: scrollPositionLicna}, 600); //scroll left
+                }
+            });
 
-        $(".tripleCarousel.licna .carousel-control-prev").on("click", function () {
-            if (scrollPositionLicna > 0) {
-                scrollPositionLicna -= cardWidthLicna;
-                $(".tripleCarousel.licna .carousel-inner").animate(
-                    {scrollLeft: scrollPositionLicna},
-                    600
-                );
-            }
-        });
+            $(".tripleCarousel.licna .carousel-control-prev").on("click", function () {
+                if (scrollPositionLicna > 0) {
+                    scrollPositionLicna -= cardWidthLicna;
+                    $(".tripleCarousel.licna .carousel-inner").animate(
+                        {scrollLeft: scrollPositionLicna},
+                        600
+                    );
+                }
+            });
+        }
+        if ($(".tripleCarousel.izdate .carousel-inner")) {
+            //izdate knjige
+            var carouselWidthIzdate = $(".tripleCarousel.izdate .carousel-inner")[0].scrollWidth;
+            var cardWidthIzdate = $(".tripleCarousel.izdate .carousel-item").width();
 
-        //izdate knjige
-        var carouselWidthIzdate = $(".tripleCarousel.izdate .carousel-inner")[0].scrollWidth;
-        var cardWidthIzdate = $(".tripleCarousel.izdate .carousel-item").width();
+            scrollPositionIzdate = 0;
 
-        scrollPositionIzdate = 0;
+            $(".tripleCarousel.izdate .carousel-control-next").on("click", function () {
+                if (scrollPositionIzdate < (carouselWidthIzdate - cardWidthIzdate * 4)) { //check if you can go any further
+                    scrollPositionIzdate += cardWidthIzdate;  //update scroll position
+                    $(".tripleCarousel.izdate .carousel-inner").animate({scrollLeft: scrollPositionIzdate}, 600); //scroll left
+                }
+            });
 
-        $(".tripleCarousel.izdate .carousel-control-next").on("click", function () {
-            if (scrollPositionIzdate < (carouselWidthIzdate - cardWidthIzdate * 4)) { //check if you can go any further
-                scrollPositionIzdate += cardWidthIzdate;  //update scroll position
-                $(".tripleCarousel.izdate .carousel-inner").animate({scrollLeft: scrollPositionIzdate}, 600); //scroll left
-            }
-        });
-
-        $(".tripleCarousel.izdate .carousel-control-prev").on("click", function () {
-            if (scrollPositionIzdate > 0) {
-                scrollPositionIzdate -= cardWidthIzdate;
-                $(".tripleCarousel.izdate .carousel-inner").animate(
-                    {scrollLeft: scrollPositionIzdate},
-                    600
-                );
-            }
-        });
-
-
+            $(".tripleCarousel.izdate .carousel-control-prev").on("click", function () {
+                if (scrollPositionIzdate > 0) {
+                    scrollPositionIzdate -= cardWidthIzdate;
+                    $(".tripleCarousel.izdate .carousel-inner").animate(
+                        {scrollLeft: scrollPositionIzdate},
+                        600
+                    );
+                }
+            });
+        }
     } else {
         $(tripleCarouselElementLicna).addClass("slide");
         $(tripleCarouselElementIzdate).addClass("slide");
